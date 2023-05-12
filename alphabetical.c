ea248c83984abb197b63e576b9bdc12e8ee396e8
TomasNagy <tomi.nagy@outlook.com>
2023-05-12 22:41:01 +0200

    Add and correct comments

--- alphabetical.c
+++ alphabetical.c
@@ -16,9 +16,13 @@ int main(void)
 // Check if all the characters are lowercase and in alphabetical order
 int check_string(string anystring)
 {
-	// Iterate through the string, we should stop 
+	// Iterate through the string, get length of the given string with strlen from string.h library   
+	// because we compare the i and i+1 characters in the string we have to loop until the last but one  
+	
 	for(int i = 0, length = strlen(anystring) - 1; i < length; i++)	
 	{
+		// if character is not after 'a' and 
+		// character i decimal value is bigger then character i+1 (f.e. 'ea' 'e'=101, 'a'=97) 
 		if (anystring[i] < 'a' || (anystring[i] > anystring[i+1]))
 		{
 			printf("No\n");
